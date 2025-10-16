9c05ff7ac9d99f759d1845be2c51284a8a517b83
trifajarsuryani <EMAIL_GITHUB_KAMU>
2025-10-16 21:36:27 +0700

    push update

---
+++ app/Http/Controllers/BeritaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BeritaController extends Controller
+{
+    public function index() {
+        return view('berita',  [
+            "title" => "Berita",
+            "berita" => Berita::ambildata(),
+
+        ])
+    }
+
+    public function tampildata($slugp) {
+
+        return view('singleberita', [
+            "title" => "Berita",
+            "new_berita" => Berita::caridata($slugp),
+        ]);
+    }
+}
